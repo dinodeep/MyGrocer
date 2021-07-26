b4817141afc2ff62d0221d0ad6e83f0abbc859c2
Deeplo0905 <68748791+Deeplo0905@users.noreply.github.com>
2021-07-26 15:16:13 -0400

    made easier to provide date input to grocer
    needs testing, but date now accepted as YYYY-MM-DD, MM-DD, or
    the first three letters of the day

--- classes/grocer.py
+++ classes/grocer.py
@@ -7,6 +7,8 @@ from classes.cookbook import Cookbook
 import datetime
 import os
 
+day_str = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+
 
 def clear():
     ''' clears the terminal screen '''
@@ -21,6 +23,43 @@ def isisoformat(date_str):
         len(parts[0]) == 4 and parts[0].isnumeric() and \
         len(parts[1]) == 2 and parts[1].isnumeric() and 1 <= int(parts[1]) <= 12 and \
         len(parts[2]) == 2 and parts[2].isnumeric() and 1 <= int(parts[2]) <= 31
+
+
+def isshortformat(date_str):
+    ''' returns True if the date_str is a shorter format 'MM-DD' '''
+    parts = date_str.lower().split("-")
+    return len(parts) == 2 and \
+        len(parts[0]) == 2 and parts[0].isnumeric() and 1 <= int(parts[1]) <= 12 and \
+        len(parts[1]) == 2 and parts[1].isnumeric() and 1 <= int(parts[1]) <= 31
+
+
+def isdayformat(date_str):
+    ''' returns True if the date_str is in the day format as Mon, Tue, Wed, Thu, Fri, Sat, Sun, False otherwise '''
+    return date_str.lower() in day_str
+
+
+def convertdate(date_str):
+    ''' converts the given date string to the next satisfying date including this date '''
+    today = datetime.date.today()
+
+    if isisoformat(date_str):
+        return datetime.date.fromisoformat(date_str)
+
+    if isshortformat(date_str):
+        date = datetime.date.fromisoformat(f"{today.year}-{date_str}")
+        # set to next year if necessary
+        if date < today:
+            date.year += 1
+        return date
+
+    if isdayformat(date_str):
+        day_idx = day_str.index(date_str)
+        today_idx = today.weekday()
+        day_delta = day_idx - today_idx
+        day_delta = day_delta if day_delta >= 0 else day_delta + 7
+        return today + datetime.timedelta(days=day_delta)
+
+    return None
 
 
 class Grocer:
@@ -55,9 +94,11 @@ class Grocer:
         ''' asks for user input to choose the recipe and provide a date and parse it '''
 
         def well_formated(plan) -> bool:
-            return len(plan) == 2 and plan[0].isnumeric() and 1 <= int(plan[0]) <= len(self.cookbook.recipes) and isisoformat(plan[1])
+            return len(plan) == 2 and plan[0].isnumeric() and 1 <= int(plan[0]) <= len(self.cookbook.recipes) and \
+                convertdate(plan[1]) is not None
 
-        print("Enter the number of the recipe and date in YYYY-MM-DD format (type DONE to finish planning)")
+        print("Enter the number of the recipe and date format (type DONE to finish planning)")
+        print("The date can be: YYYY-MM-DD, MM-DD, or the first three letters of a day (ex: Mon or Wed)")
         print("Example: 3 2021-04-09")
 
         plan = []
@@ -75,7 +116,7 @@ class Grocer:
 
         sorted_recipes = sorted(self.cookbook.recipes, key=lambda recipe: recipe.last_eaten)
         recipe = sorted_recipes[int(recipe_idx) - 1]
-        date = datetime.date.fromisoformat(date_str)
+        date = convertdate(date_str)
 
         return True, recipe, date
 
